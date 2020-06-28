01e74a2e3c94ab63251264591b677613121cf679
sahaja <sahaja@gmail.com>
2020-06-28 16:08:40 +0530

    Updates

---
+++ precache-manifest.cdee9893ac26beb478e82f0742720ed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae35f277c89cb0f44126979ddb2f1726",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "bb87915c9e6b5c1681c4",
+    "url": "/Pokemon/static/css/main.9e9e0ebf.chunk.css"
+  },
+  {
+    "revision": "e4e26a835431f68325b6",
+    "url": "/Pokemon/static/js/2.2d82bf5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon/static/js/2.2d82bf5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb87915c9e6b5c1681c4",
+    "url": "/Pokemon/static/js/main.0a39fede.chunk.js"
+  },
+  {
+    "revision": "88f10aed6ed0ea6669b1",
+    "url": "/Pokemon/static/js/runtime-main.74e75df6.js"
+  }
+]);
